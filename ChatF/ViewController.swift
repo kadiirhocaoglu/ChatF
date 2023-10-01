ca10d26084420b00a9b57e4ed6df0679930dfee9
kadiirhocaoglu <kadiirhocaoglu@hotmail.com>
2023-10-01 13:10:50 +0300

    Getting Started

---
+++ ChatF/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatF
+//
+//  Created by Kadir Hocaoğlu on 1.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
