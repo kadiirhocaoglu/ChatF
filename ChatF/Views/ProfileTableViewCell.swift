d4a34623269d15debeebeb04a0111f1009512e72
kadiirhocaoglu <kadiirhocaoglu@hotmail.com>
2023-10-04 18:28:54 +0300

    gitignored

---
+++ ChatF/Views/ProfileTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileTableViewCell.swift
+//  ChatF
+//
+//  Created by Kadir Hocaoğlu on 4.10.2023.
+//
+
+import UIKit
+
+class ProfileTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
